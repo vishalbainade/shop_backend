34106a674ed12ef4ffa12a308864c2dc8462ae17
vishalbainade <vishalbainade27@gmail.com>
2025-07-12 17:42:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -57,6 +57,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log("Connecting to DB with:", {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
@@ -64,9 +72,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
+    require: true,
     rejectUnauthorized: false,
-    require: true
-  }
+  },
 });
 
 pool.connect()
