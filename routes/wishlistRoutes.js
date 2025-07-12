968a2b33c2ca276ac51ace1648242677f5926910
vishalbainade <vishalbainade27@gmail.com>
2025-07-12 15:20:33 +0530

    Update wishlistRoutes.js

--- routes/wishlistRoutes.js
+++ routes/wishlistRoutes.js
@@ -17,7 +17,7 @@
 
 const express = require('express');
 const pool = require('../db');
-const { verifyToken } = require('../middleware/auth');
+const { verifyToken } = require('../middlewares/auth');
 const router = express.Router();
 
 // Get wishlist items
